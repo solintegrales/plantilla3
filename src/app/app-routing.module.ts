0d2fa60cef0726aec4139b8b1e4d91d2f88ebf98
Andres Parra <solintegralesei@gmail.com>
2021-10-28 11:52:41 -0500

    plantilla 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NoencontroComponent } from './404/noencontro/noencontro.component';
+import { LoginComponent } from './auth/login/login.component';
+import { DashboardComponent } from './paginas/dashboard/dashboard.component';
+import { PrincipalComponent } from './paginas/principal.component';
 
-const routes: Routes = [];
+const ruta: Routes = [
+  {
+    path: '', component: PrincipalComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    ]
+  },
+  { path: 'login', component: LoginComponent },
+  { path: '**', component: NoencontroComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(ruta)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
